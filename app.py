32fae971c70e902a3d7d39c8c57855d37112c3ad
D-Sebastian-Ch-Y <138860789+D-Sebastian-Ch-Y@users.noreply.github.com>
2023-11-29 21:38:36 +0000

    Creación de app en python para crear un juego de piedra pepel y tijeras :v

--- app.py
+++ app.py
@@ -3,9 +3,47 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
-app = Flask(__name__)
+import random
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+def obtener_opcion_aleatoria():
+    opciones = ['piedra', 'papel', 'tijeras']
+    return random.choice(opciones)
+
+def determinar_ganador(jugador, oponente):
+    if jugador == oponente:
+        return "Empate"
+    elif (jugador == 'piedra' and oponente == 'tijeras') or \
+         (jugador == 'tijeras' and oponente == 'papel') or \
+         (jugador == 'papel' and oponente == 'piedra'):
+        return "¡Has ganado!"
+    else:
+        return "¡Has perdido!"
+
+def jugar():
+    puntos_jugador = 0
+
+    while True:
+        print("\nElige: piedra, papel o tijeras. Para salir, escribe 'salir'.")
+        eleccion = input("Tu elección: ").lower()
+
+        if eleccion in ['piedra', 'papel', 'tijeras']:
+            opcion_oponente = obtener_opcion_aleatoria()
+            resultado = determinar_ganador(eleccion, opcion_oponente)
+            print(f"\nElegiste: {eleccion}. El oponente eligió: {opcion_oponente}. {resultado}")
+
+            if resultado == "¡Has ganado!":
+                puntos_jugador += 1
+            elif resultado == "¡Has perdido!":
+                puntos_jugador -= 1
+
+            print(f"Puntos: {puntos_jugador}")
+        elif eleccion == 'salir':
+            break
+        else:
+            print("Opción no válida. Por favor, elige piedra, papel o tijeras.")
+
+    print("\n¡Gracias por jugar!")
+    print(f"Tu puntuación final es: {puntos_jugador}")
+
+if __name__ == "__main__":
+    jugar()
